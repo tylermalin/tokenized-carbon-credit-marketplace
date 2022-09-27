4eee1b1c649d3e3d3ef2bf36814ed4729d721533
Ty Malin <tylermalin@gmail.com>
2022-09-27 12:34:53 -0700

    Update ipfsApi.js

--- frontend/src/components/ipfs/ipfsApi.js
+++ frontend/src/components/ipfs/ipfsApi.js
@@ -1,6 +1,6 @@
 //using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server. See IPFS.io docs
-const IPFS = require('ipfs-api');
 
 // const ipfsApi = require('ipfs-api');
 
 export default ipfs; 
+export default ipfs;
