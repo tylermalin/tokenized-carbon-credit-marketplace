a5d1652df32c82bde55b473cda151b6d32e6f7a3
Ty Malin <tylermalin@gmail.com>
2022-09-27 12:36:26 -0700

    Update ipfsApi.js
    chages

--- frontend/src/components/ipfs/ipfsApi.js
+++ frontend/src/components/ipfs/ipfsApi.js
@@ -1,4 +1,3 @@
-//using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server. See IPFS.io docs
 const IPFS = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 const ipfsApi = require('ipfs-api');
 const ipfs = new ipfsApi('localhost', '5001', {protocol: 'http'});
